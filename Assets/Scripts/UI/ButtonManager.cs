d0ece92fd8e1df90bb6e61ef17e71b443b026f60
Specu3800 <msuliborski@gmail.com>
2018-04-21 15:42:32 +0200

    feat: menu structure

---
+++ Assets/Scripts/UI/ButtonManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+	public void changeScene (string sceneName) {
+		SceneManager.LoadScene(sceneName);
+	}
+
+	public void exitApplication() {
+		Application.Quit();
+	}
+
+}
